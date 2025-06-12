c4c96e0c2a3540e1e817f73593e0d33fc1a32c3b
Ryan Carvalho Bernardo <106412618+Ryaannsz@users.noreply.github.com>
2025-06-12 10:33:04 -0300

    fix: fix api port

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -2,17 +2,18 @@ import userService from '../services/user.service.js';
 
 
 const saveEntity = async (req, res) => {
-    if(!req.body || !req.body.name || !req.body.password || !req.body.email) 
-        return res.status(400).json({message: "Faltando informações."});
+    console.log("oii")
+    if (!req.body || !req.body.name || !req.body.password || !req.body.email)
+        return res.status(400).json({ message: "Faltando informações." });
     userService.register(req, res);
 }
 
 const loginEntity = async (req, res) => {
-    
-    if(!req.body || !req.body.password || !req.body.email) 
-        return res.status(400).json({message: "Faltando informações."});
+
+    if (!req.body || !req.body.password || !req.body.email)
+        return res.status(400).json({ message: "Faltando informações." });
 
     userService.login(req, res);
 }
 
-export default {saveEntity, loginEntity}
+export default { saveEntity, loginEntity }
